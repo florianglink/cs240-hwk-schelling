0d4f0ef64bc6d3b3e5572c8b22752c098b619828
Florian Godfrey Link <fgodfreylink@pugetsound.edu>
2021-10-27 14:45:10 -0700

    created table class

--- schelling.js
+++ schelling.js
@@ -1,4 +1,24 @@
 /**
  * Schelling's Model simulator
- * @author
+ * @author Florian Godfrey Link
  */
+
+class schellingTable {
+    this.dimension = document.querySelector(#dimension).value;
+    this.threshold = document.querySelector(#threshold).value;
+    this.vacancy = document.querySelector(#vacantRatio).value;
+    this.ratio = document.querySelector(#popRatio).value;
+    this.popXcolor = document.querySelector(#popXcolor).value;
+    this.popYcolor = document.querySelector(#popYcolor).value;
+
+    intstantiate() {
+        var table = [];
+        for(var i=0; i<this.dimension; i++) {
+            table[i] = [];
+            for(var j=0; j<this.dimension; j++) {
+                table[i][j] = -1;
+            }
+        }
+    }
+
+}
