d810f696b117058105a2f2d3d9ec4eb26c91a79f
Florian Godfrey Link <fgodfreylink@pugetsound.edu>
2021-10-28 13:49:50 -0700

    added event listener to dynamically change the state of the board, working on fixing the color listeners

--- schelling.js
+++ schelling.js
@@ -11,46 +11,118 @@ class schellingTable {
     ratio = document.querySelector("#popRatio").value;
     popXcolor = document.querySelector("#popXcolor").value;
     popYcolor = document.querySelector("#popYcolor").value;
+    table = [];
 
     instantiate() {
-        var table = [];
         for(var i=0; i<this.dimension; i++) {
-            table[i] = [];
+            this.table[i] = [];
             for(var j=0; j<this.dimension; j++) {
-                table[i][j] = -1;
-            }
-        }
-        this.table = table;
-    }
-
-}
-
-function makeTable(t) {
-    var board = document.querySelector("#board");
-    board.firstElementChild && board.removeChild(board.firstElementChild);
-    table = document.createElement("table");
-    board.appendChild(table);
-    for(var i=0; i<t.dimension; i++) {
-        var row = document.createElement("tr");
-        table.appendChild(row);
-        for(j=0; j<t.dimension; j++) {
-            var data = document.createElement("td");
-            if(Math.random() < t.vacancy) {
-                data.style.backgroundColor = "#FFFFFF";
-            }
-            else {
-                if(Math.random() < t.ratio) {
-                    data.style.backgroundColor = t.popXcolor;
+                if(Math.random() < this.vacancy) {
+                    this.table[i][j] = "x";
                 }
                 else {
-                    data.style.backgroundColor = t.popYcolor;
+                    if(Math.random() < this.ratio) {
+                        this.table[i][j] = "a";
+                    }
+                    else {
+                        this.table[i][j] = "b";
+                    }
                 }
             }
-            row.appendChild(data);
         }
+    }
+
+    makeTable() {
+        var board = document.querySelector("#board");
+        board.firstElementChild && board.removeChild(board.firstElementChild);
+        var table1 = document.createElement("table");
+        board.appendChild(table1);
+        for(var i=0; i<this.dimension; i++) {
+            var row = document.createElement("tr");
+            table1.appendChild(row);
+            for(var j=0; j<this.dimension; j++) {
+                var data = document.createElement("td");
+                if(this.table[i][j] == "x") {
+                    data.style.backgroundColor = "#FFFFFF";
+                }
+                else if(this.table[i][j] == "a") {
+                    data.style.backgroundColor = this.popXcolor;
+                }
+                else if(this.table[i][j] == "b") {
+                    data.style.backgroundColor = this.popYcolor;
+                }
+                row.appendChild(data);
+            }
+        }
+    }
+    listeners() {
+        document.querySelector("#randomize").addEventListener("click", function() {
+            startup();
+        });
+        var dim = document.querySelector("#dimension");
+        dim.addEventListener("input", function() {
+            dim.value = dim.value;
+            startup();
+        });
+        var thresh = document.querySelector("#threshold");
+        thresh.addEventListener("input", function() {
+            thresh.value = thresh.value;
+        });
+        var vacant = document.querySelector("#vacantRatio");
+        vacant.addEventListener("input", function() {
+            vacant.value = vacant.value;
+            startup();
+        });
+        var pop = document.querySelector("#popRatio");
+        pop.addEventListener("input", function() {
+            pop.value = pop.value;
+            startup();
+        });
+        var xColor = document.querySelector("#popXcolor");
+        xColor.addEventListener("input", function() {
+            popXcolor.value = popXcolor.value;
+            startup();
+        })
     }
 }
 
-let schelling = new schellingTable;
-schelling.instantiate();
-makeTable(schelling);
+
+// function listeners() {
+//     document.querySelector("#randomize").addEventListener("click", function() {
+//         startup();
+//     });
+//     var dim = document.querySelector("#dimension");
+//     dim.addEventListener("input", function() {
+//         dim.value = dim.value;
+//         startup();
+//     });
+//     var thresh = document.querySelector("#threshold");
+//     thresh.addEventListener("input", function() {
+//         thresh.value = thresh.value;
+//     });
+//     var vacant = document.querySelector("#vacantRatio");
+//     vacant.addEventListener("input", function() {
+//         vacant.value = vacant.value;
+//         startup();
+//     });
+//     var pop = document.querySelector("#popRatio");
+//     pop.addEventListener("input", function() {
+//         pop.value = pop.value;
+//         startup();
+//     });
+//     var xColor = document.querySelector("#popXcolor");
+//     xColor.addEventListener("input", function() {
+//         popXcolor.value = popXcolor.value;
+//         this.table.makeTable();
+//     })
+// }
+
+//listeners();
+startup();
+    
+function startup() {
+    let schelling = new schellingTable;
+    schelling.instantiate();
+    schelling.makeTable();
+    schelling.listeners();
+}
